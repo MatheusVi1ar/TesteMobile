1ea701a1825735c77128fccd08e8f99878a6bc92
Matheus <matheusvilar8@gmail.com>
2020-08-24 21:37:07 -0300

    Commit

---
+++ TesteMobile/TesteMobile/ViewModels/AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TesteMobile.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "GitHub";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/MatheusVi1ar"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ TesteMobile/TesteMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TesteMobile.Models;
+
+namespace TesteMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
